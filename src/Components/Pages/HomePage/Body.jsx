91bdaca35733ccc91592eced7273391bf54eca6e
Neeraj957 <neerajnani.14@gmail.com>
2023-12-27 18:58:34 +0530

    Homepage, Navbar, Footer

---
+++ src/Components/Pages/HomePage/Body.jsx
@@ -0,0 +1,48 @@
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import cardImage1 from './Images/Card-Image-1.png';
+import cardImage2 from './Images/Card-Image-2.png';
+
+
+export function Body() {
+    return (
+        <Container fluid className="px-2">
+            <Row className="my-4">
+                <Col lg={12}>
+                    <Card>
+                        <Card.Body>
+                            <Row>
+                                <Col lg={4}>
+                                    <img src={cardImage1} alt="Image" className="img-fluid" />
+                                </Col>
+                                <Col lg={8}>
+                                    <h5>Welcome to task management system.</h5>
+                                    <p>Content text goes here.</p>
+                                </Col>
+                            </Row>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+
+            <Row className="my-4">
+                <Col lg={12}>
+                    <Card>
+                        <Card.Body>
+                            <Row>
+                                <Col lg={8}>
+                                    <h5>Handle Multiple Projects Efficiently.</h5>
+                                    <p>Content text goes here.</p>
+                                </Col>
+                                <Col lg={4}>
+                                    <img src={cardImage2} alt="Image" className="img-fluid" />
+                                </Col>
+                            </Row>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Body;
---
+++ src/Components/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import Body from "./Body";
+import HomepageCarousel from "./Carousel";
+
+function HomePage() {
+    return(
+        <div id="home">
+            <HomepageCarousel></HomepageCarousel>
+            <Body></Body>
+        </div>
+    )
+}
+
+export default HomePage;
