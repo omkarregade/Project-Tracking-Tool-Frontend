4e92553cfa73efe14d53b6409f56e0e8c6fbf544
FlashShri <shrikantdhengle3101@gmail.com>
2024-02-15 19:36:09 +0530

    version 1.2, changing some

--- src/Components/Pages/EmployeeDashboard/EmployeeProfile.jsx
+++ src/Components/Pages/EmployeeDashboard/EmployeeProfile.jsx
@@ -1,109 +1,119 @@
-import React, { useState, useEffect } from 'react';
-import { Modal, Button, Form } from 'react-bootstrap';
-import '../../CssFiles/EmployeeProfile.css'; // Adjust the path according to your project structure
-import {getEmployeeById, updateEmployee} from '../../Service/EmployeeService'; // Import the EmployeeService functions
+import React, { useState, useEffect } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+import "../../CssFiles/EmployeeProfile.css"; // Adjust the path according to your project structure
+import { getEmployeeById, updateEmployee } from "../../Service/EmployeeService"; // Import the EmployeeService functions
 import { getId } from "../../Service/Util";
 
-
 const EmployeeProfile = () => {
-    const [employeeData, setEmployeeData] = useState({
-        employeeId:'',
-        fullName: '',
-        email: '',
-        phoneNumber: '',
-        city: ''
-    });
+  const [employeeData, setEmployeeData] = useState();
 
-    const [isEditing, setIsEditing] = useState(false);
-    const [editedProfile, setEditedProfile] = useState({ ...employeeData });
+  // {
+  //     employeeId:'',
+  //     fullName: '',
+  //     email: '',
+  //     phoneNumber: '',
+  //     city: ''
+  // }
 
-    const fetchEmployeeProfile = async () => {
-        try {
-            const id = getId('id');
-            const response = await getEmployeeById(id);
-            setEmployeeData(response.data);
-            setEditedProfile(response.data);
-        } catch (error) {
-            console.error('Error fetching employee profile:', error);
-        }
-    };
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedProfile, setEditedProfile] = useState({ ...employeeData });
 
-    useEffect(() => {
-        fetchEmployeeProfile();
-    }, []);
+  const fetchEmployeeProfile = async () => {
+    try {
+      const id = getId("id");
+      const response = await getEmployeeById(id);
+      setEmployeeData(response.data);
+      setEditedProfile(response.data);
 
-    const handleEditProfile = () => {
-        setIsEditing(true);
-    };
+      console.log(response);
+    } catch (error) {
+      console.error("Error fetching employee profile:", error);
+    }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const id = localStorage.getItem('id');
-            await updateEmployee(editedProfile);
-            setEmployeeData(editedProfile);
-            setIsEditing(false);
-        } catch (error) {
-            console.error('Error updating employee profile:', error);
-        }
-    };
+  useEffect(() => {
+    fetchEmployeeProfile();
+  }, []);
 
-    const handleCancelEdit = () => {
-        setEditedProfile(employeeData);
-        setIsEditing(false);
-    };
+  const handleEditProfile = () => {
+    setIsEditing(true);
+  };
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setEditedProfile((prevProfile) => ({
-            ...prevProfile,
-            [name]: value,
-        }));
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const id = localStorage.getItem("id");
+      await updateEmployee(editedProfile);
+      setEmployeeData(editedProfile);
+      setIsEditing(false);
+    } catch (error) {
+      console.error("Error updating employee profile:", error);
+    }
+  };
 
-    return (
-        <div className="employee-profile my-5">
-            <div className="employee-profile-header">
-                <h1>{employeeData.fullName}</h1>
-            </div>
-            <div className="employee-profile-details">
-                <p>ID: {employeeData.employeeId}</p>
-                <p>Email: {employeeData.email}</p>
-                <p>Phone: {employeeData.phoneNumber}</p>
-                <p>Address: {employeeData.city}</p>
-            </div>
-            <button className="employee-profile-button mt-3" onClick={handleEditProfile}>
-                Edit Profile
-            </button>
+  const handleCancelEdit = () => {
+    setEditedProfile(employeeData);
+    setIsEditing(false);
+  };
 
-            <Modal show={isEditing} onHide={handleCancelEdit} className="edit-profile-modal">
-                <Modal.Header closeButton>
-                    <Modal.Title>Edit Profile</Modal.Title>
-                </Modal.Header>
-                <Modal.Body className="p-4">
-                    <Form onSubmit={handleSubmit} className="edit-profile-form">
-                        <Form.Group className="mb-3">
-                            <Form.Label>Name:</Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="name"
-                                value={editedProfile.name}
-                                onChange={handleInputChange}
-                            />
-                        </Form.Group>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleCancelEdit}>
-                        Cancel
-                    </Button>
-                    <Button variant="primary" type="submit" onClick={handleSubmit}>
-                        Save
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </div>
-    );
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setEditedProfile((prevProfile) => ({
+      ...prevProfile,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div className="employee-profile my-5">
+      <div className="employee-profile-header">
+        <h1>{employeeData.fullName}</h1>
+      </div>
+      <div className="employee-profile-details">
+        <p>ID: {employeeData.employeeId}</p>
+        <p>Email: {employeeData.email}</p>
+        <p>Phone: {employeeData.phoneNumber}</p>
+        <p>Address: {employeeData.city}</p>
+      </div>
+      <button
+        className="employee-profile-button mt-3"
+        onClick={handleEditProfile}
+      >
+        Edit Profile
+      </button>
+
+      <Modal
+        show={isEditing}
+        onHide={handleCancelEdit}
+        className="edit-profile-modal"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Profile</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="p-4">
+          <Form onSubmit={handleSubmit} className="edit-profile-form">
+            <Form.Group className="mb-3">
+              <Form.Label>Name:</Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                value={editedProfile.name}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCancelEdit}>
+            Cancel
+          </Button>
+          <Button variant="primary" type="submit" onClick={handleSubmit}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 };
 
 export default EmployeeProfile;
--- src/Components/Service/ProjectService.js
+++ src/Components/Service/ProjectService.js
@@ -1,58 +1,72 @@
-import axios from 'axios';
+import axios from "axios";
 
 import { BASE_URL } from "../Service/APIConstant";
 
-
 export async function addProject(project) {
-    try {
-        const response = await axios.post(`${BASE_URL}/project`, project);
-        return response.data;
-    } catch (error) {
-        throw error.response.data;
-    }
+  try {
+    const response = await axios.post(`${BASE_URL}/project`, project);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
 }
 
 export async function getProjectById(projectId) {
-    try {
-        const response = await axios.get(`${BASE_URL}/project/${projectId}`);
-        return response.data;
-    } catch (error) {
-        throw error.response.data;
-    }
+  try {
+    const response = await axios.get(`${BASE_URL}/project/${projectId}`);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
 }
 
 export async function getAllProjects() {
-    try {
-        const response = await axios.get(`${BASE_URL}/projects`);
-        return response.data;
-    } catch (error) {
-        throw error.response.data;
-    }
+  try {
+    const response = await axios.get(`${BASE_URL}/projects`);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
 }
 
 export async function deleteProject(projectId) {
-    try {
-        const response = await axios.delete(`${BASE_URL}/project/${projectId}`);
-        return response.data;
-    } catch (error) {
-        throw error.response.data;
-    }
+  try {
+    const response = await axios.delete(`${BASE_URL}/project/${projectId}`);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
 }
 
 export async function updateProject(projectId, updatedProject) {
-    try {
-        const response = await axios.put(`${BASE_URL}/project/${projectId}`, updatedProject);
-        return response.data;
-    } catch (error) {
-        throw error.response.data;
-    }
+  try {
+    const response = await axios.put(
+      `${BASE_URL}/project/${projectId}`,
+      updatedProject
+    );
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+}
+
+export async function assignProject(projectId, managerId) {
+  try {
+    console.log(projectId, managerId);
+    const response = await axios.post(
+      `${BASE_URL}/${projectId}/assign-manager/${managerId}`
+    );
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
 }
 
 export async function getProjectsByEmployeeId(employeeId) {
-    try {
-        const response = await axios.get(`${BASE_URL}/employees/${employeeId}`);
-        return response.data;
-    } catch (error) {
-        throw error.response.data;
-    }
+  try {
+    const response = await axios.get(`${BASE_URL}/employees/${employeeId}`);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
 }
